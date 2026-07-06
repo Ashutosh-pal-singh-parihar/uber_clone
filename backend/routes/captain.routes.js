b01bf6f3a388cca0c7fb7d816d45659cb50896a5
ashutosh pal singh parihar <ashutoshpalips27@gmail.com>
2026-07-06 14:10:30 +0530

    login, profile and logout endpoints implemnted for captain

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { body } = require("express-validator")
 const router = express.Router()
 const captainController = require("../controllers/captain.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 
 
 router.post('/register', [
@@ -13,5 +14,14 @@ router.post('/register', [
     body('vehicle.capacity').isInt({ min : 1}).withMessage('capacity must be at least 1'),
     body('vehicle.vehicleType').isIn(['car','motorcycle','auto']).withMessage('Invalid vehicle type'),
 ], captainController.registerCaptain )
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min : 6}).withMessage("pasword must be at least be 6 char long")
+], captainController.loginCaptain)
+
+router.get('/profile',authMiddleware.authCaptain, captainController.getCaptainProfile )
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain )
 
 module.exports = router
